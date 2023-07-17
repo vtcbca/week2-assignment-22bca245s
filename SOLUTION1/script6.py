dbf1521e82f1853a0c02c194afb17aa1ad4670a2
22bca245s <127942400+22bca245s@users.noreply.github.com>
2023-07-17 21:32:44 +0530

    Update script6.py

--- SOLUTION1/script6.py
+++ SOLUTION1/script6.py
@@ -1,21 +1,6 @@
-#script 6
-def evenodd(list):
-    evenlist=[]
-    oddlist=[]
-    for i in list:
-        if (i % 2 == 0):
-            evenlist.append(i)
-        else:
-            oddlist.append(i)
-    print("even list:",evenlist)
-    print("odd list:",oddlist)
-def creatlist():
-    list=[]
-    n=int(input("enter number of limit element:"))
-    for i in range(0,n):
-        ele=int(input())
-        list.append(ele)
-        #print(list)
-creatlist()
-evenodd(list)
-    
+#Write a python script to enter any string and print only part of string. Take input of start character and end character from user.
+ str=input("Enter a string: ")   
+ start=int(input("Enter the starting index:"))
+ end=int(input("Enter the end index:"))
+ substring=str[start:end]
+ print("\n Substring which starts from",start,"index no. and ends at",end,"index no. is : ", substring)
