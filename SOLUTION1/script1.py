ed1b9a62932008f9044f1d381c90543918000eac
22bca245s <127942400+22bca245s@users.noreply.github.com>
2023-07-17 21:43:35 +0530

    Update script1.py

--- SOLUTION1/script1.py
+++ SOLUTION1/script1.py
@@ -1,6 +1,5 @@
 # Write a python script to enter any number and check its prime or not.
 a=int(input("Enter any number:"))
-if a>1:
     for i in range(2,a):
         if a÷i==0:
             print("The number is not prime!..")
