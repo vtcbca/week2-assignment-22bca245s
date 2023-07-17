5062f18574f91be8f39b3d0fb485c53caa2a64af
22bca245s <127942400+22bca245s@users.noreply.github.com>
2023-07-17 21:40:38 +0530

    Update script7.py

--- SOLUTION1/script7.py
+++ SOLUTION1/script7.py
@@ -1,6 +1,13 @@
-#script 7
-f=["program.c","stadio.c","sample.c","a.py","math.py","hpp.py"]
-f1=[s for s in f if ".c" in s]
-print(f1)
-f_replc=[s.replace(".c",".py") for s in f]
-print(f_replc)
+#Write a python script to enter any string, replace vowel with its position number.
+For Example: input: Amit
+             output:0m2t
+str=input("Enter any string:")
+v=['a','e','i','o','u','A','E','I','O','U']
+u_str=""
+for i in range (len(str)):
+  if str[i]in v:
+    u_str+=string(i)
+  else:
+    u_str+=str[i]
+    print("\n\t Inputted string is :",str)
+    print("\t After converting index number at vowels space the string is:",u_str)
