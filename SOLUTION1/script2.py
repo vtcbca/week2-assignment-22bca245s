bc4a6a2584b90dfd53793649235fdc84aade1330
22bca245s <127942400+22bca245s@users.noreply.github.com>
2023-07-17 21:23:06 +0530

    Update script2.py

--- SOLUTION1/script2.py
+++ SOLUTION1/script2.py
@@ -1,7 +1,8 @@
+#Write a python script to enter any number and print the sum of its digit.
 a=int(input("Enter any number:"))
 sum=0
 while(a>0):
     b=a%10
     sum=sum+b
     a=a/10
-print("sum of digit of no {}is{}".format(a,sum))   
+print("sum of digit of no {}is{}".format(a,sum))  
