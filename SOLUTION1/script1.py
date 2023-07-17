5cae709c903f6063f75391a50bac5d4529504228
22bca245s <127942400+22bca245s@users.noreply.github.com>
2023-07-17 21:20:38 +0530

    Update script1.py

--- SOLUTION1/script1.py
+++ SOLUTION1/script1.py
@@ -1,8 +1,9 @@
-# Write a python script to enter any number and print the sum of its digit.
+# Write a python script to enter any number and check its prime or not.
 a=int(input("Enter any number:"))
-sum=0
-while(a>0):
-    b=a%10
-    sum=sum+b
-    a=a/10
-print("sum of digit of no {}is{}"format(a,sum))    
+if a>1:
+    for i in range(2,a):
+        if a÷i==0:
+            print("The number is not prime!..")
+            break
+        else:
+            print ("The number is prime!...")
